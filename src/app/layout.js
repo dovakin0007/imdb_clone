b6cc59c7fed2448d4251f9bb4bc682cce7673770
dovakin0007 <dovakin0007@gmail.com>
2023-11-13 13:20:12 +0530

    added light and darkmode

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/Header'
 
+import Providers from './providers'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,10 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Providers>
       <Header />
       {/*NavBar*/}
       {/*SearchBox*/}
-      {children}</body>
+      {children}
+      </Providers>
+</body>
     </html>
   )
 }
