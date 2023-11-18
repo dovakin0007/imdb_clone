16d54fa936b46320449f709c0428411d103417cc
dovakin0007 <dovakin0007@gmail.com>
2023-11-18 10:55:52 +0530

    minor changes

--- src/app/components/Results.jsx
+++ src/app/components/Results.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from './Card'
 
 export default function Results({results}) {
   return <div>
     {results.map((result)=> (
-        <div key={result.id}>{result.original_title}</div>
+      <Card key={result.id} result={result} />
+        
     ))}
   </div>
 }
